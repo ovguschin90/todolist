88467b2448973a289b4e714dfee568ca69877f67
Oleg Guschin <o.guschin@corp.mail.ru>
2023-01-03 20:35:27 +0300

    add Router with regexp

--- main.go
+++ main.go
@@ -1,31 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"regexp"
 )
 
 func main() {
-	fmt.Println("Hello World!")
-	router := NewRouter()
+	router := &Router{}
+
+	//AddRoutes
+	router.AddRoute("^/$", index)
 
 	http.ListenAndServe(":8000", router)
 }
 
+type Route struct {
+	pattern *regexp.Regexp
+	handler http.HandlerFunc
+}
+
 type Router struct {
-	routes map[string]http.HandlerFunc
+	routes []*Route
 }
 
-func (r *Router) AddRoute(pattern string, handler http.HandlerFunc) {
-	r.routes[pattern] = handler
-}
-
-func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-		
-}
-
-func NewRouter() *Router {
-	return &Router{
-		routes: make(map[string]http.HandlerFunc),
+func (rt *Router) AddRoute(pattern string, handler http.HandlerFunc) {
+	route := &Route{
+		pattern: regexp.MustCompile(pattern),
+		handler: handler,
 	}
+	rt.routes = append(rt.routes, route)
+}
+
+func (rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+
+	for _, route := range rt.routes {
+		if route.pattern.MatchString(path) {
+			route.handler.ServeHTTP(w, r)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }
